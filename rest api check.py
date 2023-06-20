f0fdff2e079b471bf178ee9947c5b9ab734534b4
ishavason1@gmail.com <ishavason1@gmail.com>
2023-06-20 10:38:53 +0000

    ingest data from REST API to delta tables

--- rest api check.py
+++ rest api check.py
@@ -1,2 +1,14 @@
 # Databricks notebook source
+# Databricks notebook source
+import requests,json
+from requests.auth import HTTPBasicAuth
+from requests.auth import HTTPDigestAuth
 
+api_response = requests.get('https://api.datamuse.com/words?ml=ringing+in+the+ears')
+auth = HTTPBasicAuth('user', 'pass')
+headers = {'Authorization': 'abcde12345'}
+data = api_response.json()
+df = spark.createDataFrame(data)
+type(df)
+df.printSchema()
+df.write.saveAsTable("api_table")
